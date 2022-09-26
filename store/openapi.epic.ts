752aaf4b1f7c9e228b138c3cc84d14c01c6a50c4
Abdella Ali <abdella@rangle.io>
2022-09-26 01:55:45 -0400

    ugh

--- store/openapi.epic.ts
+++ store/openapi.epic.ts
@@ -9,7 +9,7 @@ import {
 import { Action } from "redux";
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
